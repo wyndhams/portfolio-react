28ab167ac5665dc51d1fe52921793546b3aeaba3
wyndhams <wyn.roy@hotmail.co.uk>
2023-04-02 22:01:43 +0100

    starfield reduced to reduce latency issues

--- src/components/ThreeScene.js
+++ src/components/ThreeScene.js
@@ -64,7 +64,7 @@ function Camera() {
 
 function StarryBackground() {
   const starRef = useRef();
-  const numStars = 8000;
+  const numStars = 5000;
   const starImages = [
     star1,
     star2,
