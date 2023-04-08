cff52e426475dbc7c4cc727800cecf97197e7a60
wyndhams <wyn.roy@hotmail.co.uk>
2023-04-08 10:45:28 +0100

    styling to logo buttons added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,37 +9,62 @@ const Contact = () => {
   return (
     <>
       <h1 id='title'>Contact</h1>
-      <div className='logo'>
+      <div className='logo-wrapper-1'>
         <a href='https://github.com/wyndhams' target='_blank' rel='noreferrer'>
-          <img src={GitHubLogo} alt='GitHub Icon' className='logo' />
+          <img
+            src={GitHubLogo}
+            alt='GitHub Icon'
+            className='logo'
+            id='git-logo'
+          />
         </a>
         <a
           href='https://www.linkedin.com/in/wyndham-roy/'
           target='_blank'
           rel='noreferrer'
         >
-          <img src={LinkedInLogo} alt='LinkedIn Icon' className='logo' />
+          <img
+            src={LinkedInLogo}
+            alt='LinkedIn Icon'
+            className='logo'
+            id='linkedin-logo'
+          />
         </a>
         <a
           href='mailto: wyn.roy@hotmail.co.uk'
           target='_blank'
           rel='noreferrer'
         >
-          <img src={EmailLogo} alt='LinkedIn Icon' className='logo' />
+          <img
+            src={EmailLogo}
+            alt='LinkedIn Icon'
+            className='logo'
+            id='email-logo'
+          />
         </a>
         <a
           href='https://soundcloud.com/wyndhamelliot'
           target='_blank'
           rel='noreferrer'
         >
-          <img src={SoundCloudLogo} alt='GitHub Icon' className='logo' />
+          <img
+            src={SoundCloudLogo}
+            alt='GitHub Icon'
+            className='logo'
+            id='sc-logo'
+          />
         </a>
         <a
           href='https://www.youtube.com/@swivelrecords'
           target='_blank'
           rel='noreferrer'
         >
-          <img src={YouTubeLogo} alt='LinkedIn Icon' className='logo' />
+          <img
+            src={YouTubeLogo}
+            alt='LinkedIn Icon'
+            className='logo'
+            id='yt-logo'
+          />
         </a>
       </div>
       <p>Wyndham Design</p>
