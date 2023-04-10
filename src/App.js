92456a1fdc1f73bc8de849fc5840358b760b0b66
wyndhams <wyn.roy@hotmail.co.uk>
2023-04-10 10:57:56 +0100

    redundant navbar removed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import ThreeScene from './components/ThreeScene';
 import About from './components/About';
 import Skills from './components/Skills';
-import Navbar from './components/Navbar';
 import Projects from './components/Projects';
 import Experience from './components/Experience';
 import Interests from './components/Interests';
@@ -17,7 +16,6 @@ function App() {
   return (
     <container className='page'>
       <ThreeScene />
-      <Navbar />
       <container className='body'>
         <About />
         <Skills />
